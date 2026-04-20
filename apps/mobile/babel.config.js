008e3953eb8e249f3cbc343089736889518fd67d
Shubhjn4357 <shubhamjain.com.in@gmail.com>
2026-04-20 13:28:13 +0530

    fix

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo'],
-        plugins: ['react-native-reanimated/plugin'],
+        presets: [require.resolve('babel-preset-expo')],
+        plugins: [require.resolve('react-native-reanimated/plugin')],
     };
 };
